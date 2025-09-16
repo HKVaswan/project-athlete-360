a14865546ec2e33d0cd51c110baf4bc877890b41
H.K. Vaswan <harshkplus@gmail.com>
2025-09-16 21:48:47 +0530

    Update athletesService.tsx

--- frontend/src/services/athletesService.tsx
+++ frontend/src/services/athletesService.tsx
@@ -1,1 +1,8 @@
-export const athletesService = {};
+import api from "./api";
+
+export const athletesService = {
+  getAthletes: async () => {
+    const response = await api.get('/api/athletes');
+    return response.data;
+  }
+};
