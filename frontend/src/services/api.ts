aa3a21b4282f4fad97f17d208054ab8d3523aa4d
H.K. Vaswan <harshkplus@gmail.com>
2025-09-16 18:34:07 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,2 +1,9 @@
-const api = {};
+import axios from 'axios';
+
+const API_URL = 'https://project-athlete-360.onrender.com';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
 export default api;
