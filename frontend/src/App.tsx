c8e1ce1b3bed5d9013dcc37f09d1cbd2e8df3e7d
H.K. Vaswan <harshkplus@gmail.com>
2025-09-17 14:34:53 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import Navbar from "./components/Navbar";
+
+// Public Pages
+import Pa360ElevateLandingPage from "./pages/Pa360ElevateLandingPage";
+import LoginPage from "./pages/LoginPage";
+import FeaturesPage from "./pages/FeaturesPage"; 
+
+// Protected Pages
+import DashboardPage from "./pages/DashboardPage";
 import AthletesPage from "./pages/AthletesPage";
+import CreateAthletePage from "./pages/CreateAthletePage";
 import SessionsPage from "./pages/SessionsPage";
 import AttendancePage from "./pages/AttendancePage";
 import AssessmentsPage from "./pages/AssessmentsPage";
 import InjuriesPage from "./pages/InjuriesPage";
-import LoginPage from "./pages/LoginPage";
 import PerformancePage from "./pages/PerformancePage";
-import Pa360ElevateLandingPage from "./pages/Pa360ElevateLandingPage";
-import DashboardPage from "./pages/DashboardPage";
-import FeaturesPage from "./pages/FeaturesPage"; // Added this import
 
 const App = () => {
   return (
@@ -21,14 +26,17 @@ const App = () => {
       <Navbar />
       <div className="container mx-auto p-4">
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<Pa360ElevateLandingPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/features" element={<FeaturesPage />} />
           <Route path="/elevate" element={<Pa360ElevateLandingPage />} />
           
+          {/* Protected Routes */}
           <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/features" element={<FeaturesPage />} />
             <Route path="/athletes" element={<AthletesPage />} />
+            <Route path="/create-athlete" element={<CreateAthletePage />} />
             <Route path="/sessions" element={<SessionsPage />} />
             <Route path="/attendance/:sessionId" element={<AttendancePage />} />
             <Route path="/assessments" element={<AssessmentsPage />} />
@@ -43,4 +51,5 @@ const App = () => {
   );
 };
 
-export default App;  
+export default App;
+
