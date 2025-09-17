a73707125c2b53d1283583e619941ce161006f46
H.K. Vaswan <harshkplus@gmail.com>
2025-09-17 21:11:30 +0530

    Create AuthContext.tsx

---
+++ frontend/src/AuthContext.tsx
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
