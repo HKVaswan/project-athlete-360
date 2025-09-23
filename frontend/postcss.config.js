63fd3278ac6a387d2cbb2f2093a3ec4455055c7a
H.K. Vaswan <harshkplus@gmail.com>
2025-09-23 07:57:28 +0530

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Add more PostCSS plugins here in the future if needed
+  ],
 };
