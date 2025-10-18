95395ab1757da274c558ae482311f6d31c2c74b7
H.K. Vaswan <harshkplus@gmail.com>
2025-10-18 19:17:56 +0530

    Create logger.ts

---
+++ backend/src/logger.ts
@@ -0,0 +1,16 @@
+// src/logger.ts
+import winston from "winston";
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: combine(colorize(), timestamp(), logFormat),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
